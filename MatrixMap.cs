af226a82b393bacef504b27607fd1936db018a5d
jonnew <jpnewman@mit.edu>
2020-04-29 10:12:29 -0700

    Initial commit
    - Add MatrixMap and MatSocket elements

---
+++ MatrixMap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.ComponentModel;
+using OpenCV.Net;
+
+namespace Bonsai.JonsUtils
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Map channels or columns of matrix.")]
+    public class MatrixMap
+    {
+        //Mat MatMap(Tuple<Mat, Mat> source)
+        Mat MatMap(Mat source)
+        {
+            var output = new Mat(source.Size, source.Depth, source.Channels);
+            var map = Map.Reshape(0, 1);
+
+            // I dont know wtf is going on with this
+            if (MapDimension == Dimension.ROWS)
+            {
+                for (int i = 0; i < map.Cols; i++)
+                    CV.Copy(source.GetRow((int)map[i].Val0), output.GetRow(i));
+            }
+            else
+            {
+                for (int i = 0; i < map.Cols; i++)
+                    CV.Copy(source.GetCol((int)map[i].Val0), output.GetCol(i));
+            }
+
+            return output;
+        }
+
+        public IObservable<Mat> Process(IObservable<Mat> source)
+        {
+            return source.Select(input => MatMap(input));
+        }
+
+        public enum Dimension
+        {
+            ROWS,
+            COLS
+        }
+
+        [Description("Dimension to map. (e.g. 0 = rows.")]
+        public Dimension MapDimension { get; set; } = Dimension.ROWS;
+
+        [Description("The map. This is a vector of indicies to map either the rows or colums with.")]
+        public Mat Map { get; set; }
+
+    }
+}
---
+++ OpenCVMatSocket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using OpenCV.Net;
+
+namespace Bonsai.JonsUtils
+{
+    [Description("Sends a 2D Open CV Mat to a datagram (UDP) socket.")]
+    public class OpenCVMatSocket : Sink<Mat>
+    {
+        [Description("Address")]
+        public string addr { get; set; } = "localhost";
+
+        [Description("Port number. Changing while running has no effect.")]
+        public int port { get; set; } = 5000;
+
+        // Receiver has to provide number of rows and cols
+        public override IObservable<Mat> Process(IObservable<Mat> source)
+        {
+            return Observable.Using(
+                () =>
+                {
+                    var u = new UdpClient();
+                    u.Connect(addr, port);
+                    return u;
+                },
+                u =>
+                {
+                    return source.Do(value =>
+                    {
+                        var num_bytes = value.ElementSize * value.Cols * value.Rows;
+                        var data = new byte[num_bytes];
+                        Marshal.Copy(value.Data, data, 0, num_bytes);
+                        u.Send(data, num_bytes);
+                    });
+                });
+        }
+    }
+}
